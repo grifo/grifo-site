b5269b5184101db2fc7275d31fa6a3a81d63ee1c
Renatho <renatho@gmail.com>
2017-01-05 15:35:43 -0200

    home and header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,14 @@
+// Navigation
 (() => {
-    const x = '1';
-    return `${x} dasda`;
+    const body = document.body;
+    const header = document.getElementById('header');
+    const navigationMenu = document.getElementById('navigation-menu');
+    const toggleMenu = document.getElementById('toggle-menu');
+    const sectionHome = document.getElementById('section-home');
+    const scrolledClass = '-scrolled';
+    const activeClass = '-active';
+    const scrolled = () => body.scrollTop > sectionHome.offsetHeight - header.offsetHeight;
+
+    window.addEventListener('scroll', () => header.classList.toggle(scrolledClass, scrolled()));
+    toggleMenu.addEventListener('click', () => navigationMenu.classList.toggle(activeClass));
 })();
